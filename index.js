7fff2ba37f52f4c22860b60bc34385ec59c2e597
grekulanssi <anssi.grekula@helsinki.fi>
2020-01-04 16:18:44 +0200

    3.1 puhelinluettelon backend step1, again starting a new git repo was the toughest thing. it's unbelievable how hard git is for me. but I'm getting there

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log('Warming up the server engine...')
+
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.get('/api', (req, res) => {
+    res.send('<h1>Hello World!</h1><p>This is front page of the phonebook API.')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

00d745aa76b65c427b2646bbb24232f4fcf314b4
grekulanssi <anssi.grekula@helsinki.fi>
2020-01-04 16:41:25 +0200

    3.4: puhelinluettelon backend step4 done, quite easy still as I had looked the instructions quite well

--- index.js
+++ index.js
@@ -38,12 +38,19 @@ app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(person => person.id === id)
 
-    if(person) {
+    if (person) {
         res.json(person)
     } else {
         res.status(404).end()
     }
-  })
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {

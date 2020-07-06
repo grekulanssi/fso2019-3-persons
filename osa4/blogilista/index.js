09fcb5b307095efd0b7f363f96f0be3bb6c36a5f
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-06 14:30:58 +0300

    4.1 done, took 3 hours

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,52 +1,53 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3 || process.argv.length === 4 || process.argv.length > 5) {
-    console.log('This app needs 1 or 3 arguments:')
-    console.log('[password] OR [password, name, number]')
-    console.log('Please try again')
-    process.exit(1)
-}
-
-const mongoPw = process.argv[2]
-
 const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
 })
-
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = `mongodb+srv://fullstack19:${mongoPw}@cluster0-8dujm.mongodb.net/phonebook?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+if (process.argv.length < 3) {
+    console.log('give password as argument, please')
+    process.exit(1)
+}
+const mongoPw = process.argv[2]
+const mongoUrl = `mongodb+srv://fullstack19:${mongoPw}@cluster0-8dujm.mongodb.net/blogilista?retryWrites=true&w=majority`
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
 })
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+    const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
 })
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })

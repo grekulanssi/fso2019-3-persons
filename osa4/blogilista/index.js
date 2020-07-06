9a3ac05a7fdc7fc16a86d08fa991a268d7003273
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-06 15:19:59 +0300

    4.2 done, took 30 mins

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,52 +1,10 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-const Blog = mongoose.model('Blog', blogSchema)
-/*
-if (process.argv.length < 3) {
-    logger.error('give password as argument, please')
-    process.exit(1)
-}*/
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Yay! Server running on port ${config.PORT}`)
 })

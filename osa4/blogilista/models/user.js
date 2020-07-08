b00558adec71b5a3d67a052d812e8b378e2ba9a9
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-08 13:24:41 +0300

    4.15 done and 4.16* partially done, took 2 hours

---
+++ osa4/blogilista/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: [3, 'username must be at least 3 characters long'],
+        unique: true
+    },
+    passwordHash: String,
+    name: String
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User

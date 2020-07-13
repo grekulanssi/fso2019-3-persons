f391e63e1d7b6e437ce648bd3413ff5873558263
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-13 11:33:40 +0300

    4.6* done, took 1 hour

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
     return blogs.length * 0 + 1
 }
@@ -15,8 +17,52 @@ const favoriteBlog = (blogs) => {
     }, {})
 }
 
+/*
+4.6*
+Määrittele funktio mostBlogs joka saa parametrikseen taulukollisen blogeja.
+Funktio selvittää kirjoittajan, kenellä on eniten blogeja.
+Funktion paluuarvo kertoo myös ennätysblogaajan blogien määrän:
+{
+  author: "Robert C. Martin",
+  blogs: 3
+}
+Jos ennätysblogaajia on monta, riittää että funktio palauttaa niistä jonkun.
+*/
+
+const mostBlogs = (blogs) => {
+    const authors = blogs.map(blog => {
+        return blog.author
+    })
+    let a = _(authors).countBy().entries().maxBy(_.last())
+    return {
+        author: a[0],
+        blogs: a[1]
+    }
+}
+
+
+/*
+Määrittele funktio mostLikes joka saa parametrikseen taulukollisen blogeja.
+Funktio selvittää kirjoittajan, kenen blogeilla on eniten tykkäyksiä.
+Funktion paluuarvo kertoo myös suosikkiblogaajan likejen yhteenlasketun määrän:
+{
+  author: "Edsger W. Dijkstra",
+  likes: 17
+}
+Jos suosikkiblogaajia on monta, riittää että funktio palauttaa niistä jonkun.
+*/
+
+const mostLikes = (blogs) => {
+    const authors = blogs.map(blog => {
+        return blog.author
+    })
+    return authors.sort()
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
